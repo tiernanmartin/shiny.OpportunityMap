f84b484111b86e49a64147e756539f4b94263553
Tiernan Martin <tiernan.martin@gmail.com>
2017-10-09 00:33:46 +0000

    create template functions with roxygen documentation

---
+++ R/templateUI_obj.R
@@ -0,0 +1,11 @@
+#' Shiny Module: _description here_
+#'
+#' This is the UI portion of this module.
+#' @param id Output id
+#' @param tab_name Name of the tab panel
+#' @import shiny
+#' @import htmltools
+#' @seealso \code{\link{tabItemContent_map}}
+ui_function_name <- function(id, tab_name){
+
+}
---
+++ R/template_obj.R
@@ -0,0 +1,14 @@
+#' Shiny Module: _description here_
+#'
+#' This is the server portion of this module.
+#' @param input shiny input
+#' @param output shiny output
+#' @param session shiny session
+#' @seealso \code{\link{tabItemContentUI_map}}
+#' @import leaflet
+#' @importFrom shmodules "myLflt"
+server_function_name <- function(input, output, session){
+
+  ns <- session$ns
+
+}
